ca47b861f1d97f2cd49121f7dd36d8d9cbffd070
Aleksey <kukva2019@gmail.com>
2021-09-28 23:25:15 +1000

    fix device controller

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,17 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
 const ApiError = require("../erorr/ApiError");
-const {Device} = require("../models/models");
+const {Device, DeviceInfo} = require("../models/models");
 
 class deviceController {
-	async create(req, res) {
+	async create(req, res, next) {
 		try {
-			const {name, price, brandId, typeId, info} = req.body
+			let {name, price, brandId, typeId, info} = req.body
 			const {img} = req.files
 			let fileName = uuid.v4() + ".jpg"
 			await img.mv(path.resolve(__dirname, '..', 'static', fileName))
 			
 			const device = await Device.create({name, price, brandId, typeId, img: fileName})
+			
+			if(info){
+				info = JSON.parse(info)
+				info.forEach(i=>
+				DeviceInfo.create({
+					title: i.title,
+					description: i.description,
+					deviceId: device.id
+				}))
+			}
 			
 			return res.json(device)
 		} catch (e) {
@@ -19,25 +29,36 @@ class deviceController {
 		}
 	}
 	async getAll(req, res) {
-		const {brandId, typeId} = req.body
+		let {brandId, typeId, limit, page} = req.query
+		page = page || 1
+		limit = limit || 10
+		let offset = page * limit - limit
 		let device
-		if(!brandId && !typeId) {
 		
+		if(!brandId && !typeId) {
+			device = await Device.findAndCountAll({limit, offset})
 		}
 		if(brandId && !typeId){
-		
+			device = await Device.findAndCountAll({where:{brandId}, limit, offset})
 		}
 		if(!brandId && typeId) {
-		
+			device = await Device.findAndCountAll({where:{typeId}, limit, offset})
 		}
 		if(brandId && typeId){
-		
+			device = await Device.findAndCountAll({where:{typeId, brandId}, limit, offset})
 		}
 		
 		return res.json(device)
 	}
 	async getOn(req, res) {
-	
+		const {id} = req.params
+		const device = await Device.findOne(
+			{
+				where: {id},
+				include: [{model: DeviceInfo, as: 'info'}]
+			}
+		)
+		return res.json(device)
 	}
 }
 
