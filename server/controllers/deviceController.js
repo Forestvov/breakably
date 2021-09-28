7d185032ada8d1524d59e10063b9c2d7b7abfc50
Aleksey <kukva2019@gmail.com>
2021-09-28 20:38:47 +1000

    add device controller

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,9 +1,40 @@
+const uuid = require('uuid')
+const path = require('path')
+const ApiError = require("../erorr/ApiError");
+const {Device} = require("../models/models");
+
 class deviceController {
 	async create(req, res) {
-	
+		try {
+			const {name, price, brandId, typeId, info} = req.body
+			const {img} = req.files
+			let fileName = uuid.v4() + ".jpg"
+			await img.mv(path.resolve(__dirname, '..', 'static', fileName))
+			
+			const device = await Device.create({name, price, brandId, typeId, img: fileName})
+			
+			return res.json(device)
+		} catch (e) {
+			next(ApiError.badRequest(e.message))
+		}
 	}
 	async getAll(req, res) {
-	
+		const {brandId, typeId} = req.body
+		let device
+		if(!brandId && !typeId) {
+		
+		}
+		if(brandId && !typeId){
+		
+		}
+		if(!brandId && typeId) {
+		
+		}
+		if(brandId && typeId){
+		
+		}
+		
+		return res.json(device)
 	}
 	async getOn(req, res) {
 	
